5bf83f203ab1c119a0f9c7d6e4dcd3f4cc1cec5f
watiko <service@mail.watiko.net>
2020-08-10 16:27:30 +0900

    refactor: unify by binop

--- lib/eval.ml
+++ lib/eval.ml
@@ -2,25 +2,18 @@ open Syntax
 
 let rec eval e =
   let ename = exp_name e in
+  let binop f e1 e2 =
+    match eval e1, eval e2 with
+    | IntVal n1, IntVal n2 -> IntVal (f n1 n2)
+    | _ -> failwith "integer values expected"
+  in
   match e with
   | IntLit n -> IntVal n
   | BoolLit b -> BoolVal b
-  | Plus (e1, e2) ->
-    (match eval e1, eval e2 with
-    | IntVal n1, IntVal n2 -> IntVal (n1 + n2)
-    | _ -> failwith "integer values expected")
-  | Minus (e1, e2) ->
-    (match eval e1, eval e2 with
-    | IntVal n1, IntVal n2 -> IntVal (n1 - n2)
-    | _ -> failwith "integer values expected")
-  | Times (e1, e2) ->
-    (match eval e1, eval e2 with
-    | IntVal n1, IntVal n2 -> IntVal (n1 * n2)
-    | _ -> failwith "integer values expected")
-  | Div (e1, e2) ->
-    (match eval e1, eval e2 with
-    | IntVal n1, IntVal n2 -> IntVal (n1 / n2)
-    | _ -> failwith "integer values expected")
+  | Plus (e1, e2) -> binop ( + ) e1 e2
+  | Minus (e1, e2) -> binop ( - ) e1 e2
+  | Times (e1, e2) -> binop ( * ) e1 e2
+  | Div (e1, e2) -> binop ( / ) e1 e2
   | Eq (e1, e2) ->
     (match eval e1, eval e2 with
     | IntVal n1, IntVal n2 -> BoolVal (n1 = n2)
